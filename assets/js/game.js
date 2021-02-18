5ce8b12bfb34d2a07ab4e3da269a4b62d49f433d
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-02-18 13:00:49 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,13 +27,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         break;
       }
     }
   
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -52,7 +55,10 @@ var fight = function(enemyName) {
     }
   
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+    
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -86,7 +92,9 @@ var startGame = function() {
      var pickedEnemyName = enemyNames[i];
  
      // reset enemyHealth before starting new fight
-     enemyHealth = 50;
+     enemyHealth = randomNumber(40,60);
+
+    //  Math.floor(Math.random() * 21) + 40;
  
      // use debugger to pause script from running and check what's going on at that moment in the code
      //  debugger;
@@ -185,6 +193,13 @@ var shop = function() {
       break;
   }
 
+};
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min +1) + min);
+
+  return value;
 };
 
 // start the game when the page loads
